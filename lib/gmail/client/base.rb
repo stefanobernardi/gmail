77fb58f73f2040f100364eb6ab14cc89663697d9
Stefano Bernardi <hello@stefanobernardi.com>
2014-06-25 16:37:22 -0700

    re-fix extensions

--- lib/gmail/client/base.rb
+++ lib/gmail/client/base.rb
@@ -25,6 +25,8 @@ module Gmail
       # Connect to gmail service. 
       def connect(raise_errors=false)
         @imap = Net::IMAP.new(GMAIL_IMAP_HOST, GMAIL_IMAP_PORT, true, nil, false)
+        GmailImapExtensions.patch_net_imap_response_parser
+        @imap
       rescue SocketError
         raise_errors and raise ConnectionError, "Couldn't establish connection with GMail IMAP service"
       end
